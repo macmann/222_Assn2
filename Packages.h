e7926f9f4fa9f6d6a905373fca95030dbe9dec59
kaungsanphyoe <kaungsanphyoe@ubuntu>
2014-02-05 00:32:32 +0800

    Commit test on Packages

--- Packages.h
+++ Packages.h
@@ -5,19 +5,4 @@
  * Created on February 5, 2014, 12:10 AM
  */
 
-#ifndef PACKAGES_H
-#define	PACKAGES_H
-
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
-
-
-
-#ifdef	__cplusplus
-}
-#endif
-
-#endif	/* PACKAGES_H */
-
+#include "stdio.h"
