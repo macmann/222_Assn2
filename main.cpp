ae0db751e0fb42577b29da110b07577b59c42d65
macmann <davidkaung@gmail.com>
2014-02-08 10:20:47 -0800

    SQLITE3 create user table code added

--- main.cpp
+++ main.cpp
@@ -9,12 +9,14 @@
 #include <stdlib.h>
 #include <sqlite3.h> 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 void createTable();
 void insertTabale();
 void readTable();
+void editTable();
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
    int i;
@@ -47,11 +49,14 @@ int main(int argc, char* argv[])
                     break;
             case 3: readTable();
                     break;
+            case 4: editTable();
+                    break;
         }
      }
 
    return 0;
 }
+
 
 void createTable()
 {
@@ -61,7 +66,7 @@ void createTable()
    char *sql;
 
    /* Open database */
-   rc = sqlite3_open("user.db", &db);
+   rc = sqlite3_open("holiday.db", &db);
    if( rc ){
       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
       exit(0);
@@ -70,11 +75,13 @@ void createTable()
    }
 
    /* Create SQL statement */
-   sql = "CREATE TABLE USER("  \
-         "ID INT PRIMARY KEY     NOT NULL," \
-         "USERNAME           TEXT    NOT NULL," \
-         "LVL            INT     NOT NULL," \
-         "PASSWORD         REAL );";
+   sql = "CREATE TABLE Staff("  \
+         "staffID TEXT PRIMARY KEY," \
+         "staffFirstName TEXT NOT NULL," \
+         "staffLastName TEXT NOT NULL," \
+         "staffUserName TEXT NOT NULL," \
+         "staffPassword TEXT NOT NULL," \
+         "staffLevel TEXT NOT NULL);";
 
    /* Execute SQL statement */
    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
@@ -95,7 +102,7 @@ void insertTabale()
    char *sql;
 
    /* Open database */
-   rc = sqlite3_open("user.db", &db);
+   rc = sqlite3_open("holiday.db", &db);
    if( rc ){
       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
       exit(0);
@@ -104,10 +111,10 @@ void insertTabale()
    }
 
    /* Create SQL statement */
-   sql = "INSERT INTO USER (ID,USERNAME,LVL,PASSWORD) "  \
-         "VALUES (1, 'KSP', 1, '123456'); " \
-         "INSERT INTO USER (ID,USERNAME,LVL,PASSWORD) "  \
-         "VALUES (2, 'Vera', 2, '123456'); ";
+   sql = "INSERT INTO Staff VALUES ('S0001', 'James', 'Tan', 'james', 'james123', 1);" \
+         "INSERT INTO Staff VALUES ('S0002',  'Terence', 'Choo', 'terence', 'terence123', 2);"\
+         "INSERT INTO Staff VALUES ('S0003', 'Mary', 'Khoo', 'mary', 'mary123', 3);"\
+         "INSERT INTO Staff VALUES ('S0004', 'Sharon', 'Lee', 'sharon', 'sharon123', 4);";
    /* Execute SQL statement */
    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
    if( rc != SQLITE_OK ){
@@ -130,7 +137,7 @@ void readTable()
    const char* data = "Callback function called";
 
    /* Open database */
-   rc = sqlite3_open("user.db", &db);
+   rc = sqlite3_open("holiday.db", &db);
    if( rc ){
       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
       exit(0);
@@ -139,7 +146,44 @@ void readTable()
    }
 
    /* Create SQL statement */
-   sql = "SELECT * from USER";
+   sql = "SELECT * from Staff";
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+   if( rc != SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   }else{
+      fprintf(stdout, "Operation done successfully\n");
+   }
+   sqlite3_close(db);
+}
+
+void editTable()
+{
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+   char *sql;
+   const char* data = "Callback function called";
+
+   /* Open database */
+   rc = sqlite3_open("holiday.db", &db);
+   if( rc ){
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      exit(0);
+   }else{
+      fprintf(stderr, "Opened database successfully\n");
+   }
+
+   /* Create merged SQL statement */
+   
+   char s[20] = "teststring";
+   
+   sql = sqlite3_mprintf("UPDATE Staff set staffPassword = '%q' where staffID='S0001'; " \ 
+         "SELECT * from Staff", s);
+ //  sql = "UPDATE Staff set staffPassword = 'test' where staffID='S0001'; " \ 
+  //       "SELECT * from Staff";
 
    /* Execute SQL statement */
    rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
