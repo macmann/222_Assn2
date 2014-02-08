48ff4876aa194c8b8e273427267cf9a9280102d9
macmann <davidkaung@gmail.com>
2014-02-08 08:46:13 -0800

    SQLITE3 Integration

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 /* 
  * File:   main.cpp
  * Author: kaungsanphyoe
+ * Reference Example code used from http://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
  *
  * Created on February 5, 2014, 12:09 AM
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h> 
+#include <iostream>
 
-static int callback(void *data, int argc, char **argv, char **azColName){
+using namespace std;
+
+void createTable();
+void insertTabale();
+void readTable();
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName){
    int i;
-   fprintf(stderr, "%s: ", (const char*)data);
    for(i=0; i<argc; i++){
       printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
    }
@@ -20,14 +27,75 @@ static int callback(void *data, int argc, char **argv, char **azColName){
 
 int main(int argc, char* argv[])
 {
+    int selector; 
+    
+    while(1)
+    {
+        cout << "CSCI 222, Test Menu for Database Creation and Reading" << endl;
+        cout << "1) Create DB Called User" << endl;
+        cout << "2) Insert Data to User DB" << endl;
+        cout << "3) Read Data from User DB" << endl;
+        cout << "Enter your option: ";
+        cin >> selector;
+   
+
+        switch (selector)
+        {
+            case 1: createTable();
+                    break;
+            case 2: insertTabale();
+                    break;
+            case 3: readTable();
+                    break;
+        }
+     }
+
+   return 0;
+}
+
+void createTable()
+{
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int  rc;
+   char *sql;
+
+   /* Open database */
+   rc = sqlite3_open("user.db", &db);
+   if( rc ){
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      exit(0);
+   }else{
+      fprintf(stdout, "Opened database successfully\n");
+   }
+
+   /* Create SQL statement */
+   sql = "CREATE TABLE USER("  \
+         "ID INT PRIMARY KEY     NOT NULL," \
+         "USERNAME           TEXT    NOT NULL," \
+         "LVL            INT     NOT NULL," \
+         "PASSWORD         REAL );";
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+   if( rc != SQLITE_OK ){
+   fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   }else{
+      fprintf(stdout, "Table created successfully\n");
+   }
+   sqlite3_close(db);
+}
+
+void insertTabale()
+{
    sqlite3 *db;
    char *zErrMsg = 0;
    int rc;
    char *sql;
-   const char* data = "Callback function called";
 
    /* Open database */
-   rc = sqlite3_open("test.db", &db);
+   rc = sqlite3_open("user.db", &db);
    if( rc ){
       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
       exit(0);
@@ -36,7 +104,42 @@ int main(int argc, char* argv[])
    }
 
    /* Create SQL statement */
-   sql = "SELECT * from COMPANY";
+   sql = "INSERT INTO USER (ID,USERNAME,LVL,PASSWORD) "  \
+         "VALUES (1, 'KSP', 1, '123456'); " \
+         "INSERT INTO USER (ID,USERNAME,LVL,PASSWORD) "  \
+         "VALUES (2, 'Vera', 2, '123456'); ";
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+   if( rc != SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   }else{
+      fprintf(stdout, "Records created successfully\n");
+   }
+   sqlite3_close(db);
+}
+
+
+void readTable()
+{
+    
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+   char *sql;
+   const char* data = "Callback function called";
+
+   /* Open database */
+   rc = sqlite3_open("user.db", &db);
+   if( rc ){
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      exit(0);
+   }else{
+      fprintf(stderr, "Opened database successfully\n");
+   }
+
+   /* Create SQL statement */
+   sql = "SELECT * from USER";
 
    /* Execute SQL statement */
    rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
@@ -47,5 +150,4 @@ int main(int argc, char* argv[])
       fprintf(stdout, "Operation done successfully\n");
    }
    sqlite3_close(db);
-   return 0;
 }
