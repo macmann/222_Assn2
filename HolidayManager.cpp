072a84ebfb8af47b134e499713320c19e702bbcc
macmann <davidkaung@gmail.com>
2014-02-10 22:53:34 -0800

    Create Holiday Manager cpp

---
+++ HolidayManager.cpp
@@ -0,0 +1,21 @@
+#include "HolidayManager.h"
+
+bool HolidayManager::deleteClientAccount(string nric)
+{
+    
+};
+
+bool HolidayManager::checkSpace()
+{
+    
+}
+
+bool HolidayManager::processBooking()
+{
+    
+}
+
+void HolidayManager::HMmenu()
+{
+    
+}
