75767bc79c99459ec3b34dd453dff05f3cdba396
veraaung <aat2@np.edu.sg>
2014-02-15 03:31:47 -0800

    final client

--- FinanaceManager.h
+++ FinanaceManager.h
@@ -12,17 +12,18 @@
 
 class FinanceManager: public Staff {
     public:
-        static void fmMenu();
-        static bool viewConfirmedBooking();
-        static bool viewCancelledBooking();
-        static bool viewPaymentRequiredBooking();
-        static bool viewConfirmedBooking(string); 
-        static bool viewCancelledBooking(string);
-        static bool viewPaymentRequiredBooking(string);
-        static void processBooking();
-        static bool confirmBooking(string);
+        void fmMenu();
+        bool viewConfirmedBooking();
+        bool viewCancelledBooking();
+        bool viewPaymentRequiredBooking();
+        bool viewConfirmedBooking(string); 
+        bool viewCancelledBooking(string);
+        bool viewPaymentRequiredBooking(string);
+        void processBooking();
+        bool confirmBooking(string);
         float calculatePenalty(string);
         static int retrieveData(void *arg, int argc, char **argv, char **azColName);
 };
 
 #endif	/* FINANACEMANAGER_H */
+
